46ea4fa1d82f085dff5f5e86685aaa58682ff8fc
Matt <matthewastro@gmail.com>
2022-05-26 06:51:02 -0400

    Intialized project

---
+++ src/test/java/com/springlearning/springpetclinic/SpringpetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springlearning.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
