89c7cc8767399a9fbc0b4d0a08c9b59603a84797
Matt <matthewastro@gmail.com>
2022-06-12 20:45:45 -0400

    Added a Crud Repository

--- pet-clinic-data/src/main/java/com/springlearning/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/springlearning/springpetclinic/model/Person.java
@@ -19,4 +19,13 @@ public class Person extends BaseEntity {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    static String doubleChar(String s){
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < s.length(); i++)
+        {
+            builder.append(s.charAt(i)).append(s.charAt(i));
+        }
+        return builder.toString();
+    }
 }
---
+++ pet-clinic-data/src/test/java/com/springlearning/springpetclinic/model/PersonTest.java
@@ -0,0 +1,11 @@
+package com.springlearning.springpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+public class PersonTest {
+    @Test
+    public void getFirstNameTest_success()
+    {
+        System.out.println("Hello World!");
+    }
+
+}
